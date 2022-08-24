b4270137804b531a3578e2142ff59321c0ac4ecf
Sanja <sanja1mandic@gmail.com>
2022-08-24 20:53:14 +0200

    fix linters

--- rental.rb
+++ rental.rb
@@ -20,11 +20,11 @@ book1 = Book.new('first', 'auth1')
 person1 = Person.new(19, 'ana')
 person2 = Person.new(19, 'iva')
 
-rental1 = Rental.new(Time.now, book1, person1)
-rental1 = Rental.new(Time.now, book1, person2)
+Rental.new(Time.now, book1, person1)
+Rental.new(Time.now, book1, person2)
 
 p book1.rentals.count
-p book1.rentals.map{|r| r.date}
+p book1.rentals.map(&:date)
 
 book1.add_rental(person1, Time.now)
-p book1.rentals.map{|r| r.date}
+p book1.rentals.map(&:date)
