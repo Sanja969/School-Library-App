ade0c68da1197c650087c0054f8375084c69ac91
Sanja <sanja1mandic@gmail.com>
2022-08-24 20:39:26 +0200

    - Add initial value empty list rentals - Add method for adding rental object to the list

--- person.rb
+++ person.rb
@@ -2,13 +2,14 @@ require './nameable'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
 
   def initialize(age, name)
     @id = Time.now.to_f
     @name = name
     @age = age
     super()
+    @rentals = []
   end
 
   def can_use_services?
@@ -19,6 +20,10 @@ class Person < Nameable
     @name
   end
 
+  def add_rental(book, date)
+    Rental.new(date, book, self)
+  end
+
   private
 
   def _is_of_age?
@@ -26,9 +31,9 @@ class Person < Nameable
   end
 end
 
-person = Person.new(22, 'maximilianus')
-p person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-p capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-p capitalized_trimmed_person.correct_name
+# person = Person.new(22, 'maximilianus')
+# p person.correct_name
+# capitalized_person = CapitalizeDecorator.new(person)
+# p capitalized_person.correct_name
+# capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+# p capitalized_trimmed_person.correct_name
