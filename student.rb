f292898fb8bf643af5d877d048020c40ed8561b6
Sanja <sanja1mandic@gmail.com>
2022-08-24 19:35:14 +0200

    Add belong to relations in class Student to class Classroom and print same instance object of class

--- student.rb
+++ student.rb
@@ -1,6 +1,9 @@
 require './person'
+require './classroom'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, name, classroom)
     @classroom = classroom
     super(age, name)
@@ -9,4 +12,22 @@ class Student < Person
   def play_hooky
     '¯\(ツ)/¯'
   end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
 end
+
+class1 = Classroom.new(333)
+# p class1.students
+
+sanja = Student.new(19, 'Sanja', 22)
+class1.add_student(sanja)
+# p class1.students
+# sanja.classroom = 333
+# p class1.students
+
+sanja.classroom = 222
+
+p sanja.classroom
