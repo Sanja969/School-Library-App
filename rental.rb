5d2a0d3c9ce4701fa1c2b410ff518544fd5f4c00
Sanja <sanja1mandic@gmail.com>
2022-08-31 12:04:20 +0200

    delete some test commands from rentals

--- rental.rb
+++ rental.rb
@@ -15,16 +15,3 @@ class Rental
     person.rentals << self
   end
 end
-
-book1 = Book.new('first', 'auth1')
-person1 = Person.new(19, 'ana')
-person2 = Person.new(19, 'iva')
-
-Rental.new(Time.now, book1, person1)
-Rental.new(Time.now, book1, person2)
-
-p book1.rentals.count
-p book1.rentals.map(&:date)
-
-book1.add_rental(person1, Time.now)
-p book1.rentals.map(&:date)
